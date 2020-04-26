50ce8f9091bceb6623db839cbda1730741db30ac
daninger4995 <56229990+daninger4995@users.noreply.github.com>
2020-04-25 20:33:49 -0700

    Changed sub name to ''

--- config_example.py
+++ config_example.py
@@ -4,7 +4,7 @@ username = '' # enter user username
 password = '' # enter user password
 user_agent = 'Subreddit Stats bot by u/daninger4995 being run by u/<YOUR USERNAME HERE>' # user agent
 
-subreddit = 'cursedcomments' # name of sub
+subreddit = '' # name of sub
 
 webhookURL = '' # discord webhook URL
 
